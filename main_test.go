5dd3438c37f96651d9ccdee49f7e292d3dfd4883
Ram Lingala <srilingala1@gmail.com>
2022-04-16 23:04:35 +0100

    updated testcase 1

---
+++ main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+)
+
+func TesthomePage(t *testing.T) {
+	req, err := http.NewRequest("GET", "/homepage", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(homePage)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// Check the response body is what we expect.
+	expected := `"Welcome to the Go Web API!!!"`
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+}
